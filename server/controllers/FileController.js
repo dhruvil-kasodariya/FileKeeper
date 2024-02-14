9bae8f76e3c71aea6c9d77e1b66332ccfa9c529f
dhruvil-kasodariya <kasodariyadhruvil45@gmail.com>
2024-02-14 12:22:32 +0530

    get list of files and delete file functlity

--- server/controllers/FileController.js
+++ server/controllers/FileController.js
@@ -1,6 +1,7 @@
 const File = require("../models/File");
 
 const router = require("express").Router();
+const { unlink } = require('node:fs/promises');
 
 // Function to generate a unique 6-digit code
 const generateUniqueCode = async () => {
@@ -51,6 +52,63 @@ const addFile = async (req, res) => {
   }
 };
 
+//get files by userId
+const getUserFiles = async (req, res) => {
+  try {
+    // Extract the userId parameter from the request parameters
+    const { userId } = req.params;
+
+    // Query the database to find files belonging to the specified user
+    const filesList = await File.find({ user: userId });
+
+    // Send a JSON response with the list of files
+    return res.status(200).json(filesList);
+  } catch (error) {
+    // Handle errors
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const deleteUserFile = async (req, res) => {
+  try {
+    // Extract the fileId parameter from the request parameters
+    const { fileId } = req.params;
+
+    // Find the file by its ID
+    const getFile = await File.findById({ _id: fileId });
+
+    // Check if the file exists
+    if (!getFile) {
+      console.log("here")
+      // If the file does not exist, send a 404 Not Found response
+      return res.status(404).json({ error: "File Not Found" });
+    }
+
+    // Delete the file from the database
+    await File.findByIdAndDelete({ _id: fileId });
+
+    // Delete the file from the file system
+    try {
+      await unlink(getFile.path); // Use unlink to delete the file from the file system
+      console.log(`Successfully deleted file ${getFile.path}`);
+    } catch (error) {
+      // Handle errors that occur during file deletion from the file system
+      console.error('Error deleting file:', error.message);
+    }
+
+    // Send a success response indicating the file was deleted
+    res.status(200).send({ message: "File Deleted" });
+  } catch (error) {
+    // Handle errors
+    console.error('Error:', error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+
 module.exports = {
   addFile,
+  getUserFiles,
+  deleteUserFile
 };
--- server/routes/file.js
+++ server/routes/file.js
@@ -1,9 +1,11 @@
 
 const router = require("express").Router();
-const { addFile } = require("../controllers/FileController");
+const { addFile, getUserFiles, deleteUserFile } = require("../controllers/FileController");
 const verifyToken = require("../middleware/verifyToken");
 const uploadStorage = require("../middleware/uploadStorage");
 
 router.post("/add-file", verifyToken ,uploadStorage.single("file"), addFile);
+router.get("/get-user-files/:userId",verifyToken,getUserFiles);
+router.delete("/delete-file/:fileId",verifyToken,deleteUserFile);
 
 module.exports = router;
